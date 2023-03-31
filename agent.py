c635912375766ccf1294ff58dcf3a424e6684324
fluistro <sam674526@gmail.com>
2023-03-30 20:52:30 -0700

    added difficulties, not tested

--- agent.py
+++ agent.py
@@ -1,190 +1,227 @@
 import keras
 import numpy as np
 import chess
+import random
 
-start_model = keras.models.load_model('start_net_bigset')
-end_model = keras.models.load_model('end_net_bigset')
+print("Choose a difficulty: 0 (easy), 1 (medium), or 2 (hard)")
+difficulty = int(input())
 
-print('models loaded')
-print(start_model.summary())
+if difficulty == 0:
+    board = chess.Board()
 
-def get_net_input(fen):
-    
-    input = []
-    last = False
-    
-    for c in fen:
-        
-        # B or W to move
-        if last:
-            
-            if c == 'w':
-                input.append(0)
-            else:
-                input.append(1)
-            break
-        
-        # ignore
-        if (c == '/'):
-            continue
-        
-        # just before to move information
-        elif (c == ' '):
-            last = True
-            continue
-        
-        # data about square
-        else:
-            
-            vec = np.zeros(13) # empty square, K, Q, B, N, R, P, k, q, b, n, r, p
-            
-            # empty square
-            try:
-                
-                x = int(c)
-                
-                vec[0] = 1
-                for i in range(x):
-                    for n in vec:
-                        input.append(int(n))
-                    
-            # piece
-            except:
-                
-                if (c == 'K'):
-                    vec[1] = 1
-                elif (c == 'Q'):
-                    vec[2] = 1
-                elif (c == 'B'):
-                    vec[3] = 1
-                elif (c == 'N'):
-                    vec[4] = 1
-                elif (c == 'R'):
-                    vec[5] = 1
-                elif (c == 'P'):
-                    vec[6] = 1
-                elif (c == 'k'):
-                    vec[7] = 1
-                elif (c == 'q'):
-                    vec[8] = 1
-                elif (c == 'b'):
-                    vec[9] = 1
-                elif (c == 'n'):
-                    vec[10] = 1
-                elif (c == 'r'):
-                    vec[11] = 1
-                elif (c == 'p'):
-                    vec[12] = 1
-                
-                for n in vec:
-                    input.append(int(n))
-    
-    return input
+    print("Enter 0 if you would like to play as White, and 1 if you would like to play as Black: ")
+    colour = int(input())
 
-def get_move(board):
-    
-    wrong_start = False
-    
-    # get starting square
-    
-    fen = board.fen()
-    x_start = get_net_input(fen)
-    
-    y_start = start_model.predict([x_start])
-    start_square_list = y_start[0]
-    
-    for i in range(64):
-        x_start.append(0)
-    
-    while True:
-    
-        start_square_index = np.argmax(start_square_list)
-        start_square = chess.SQUARE_NAMES[start_square_index]
-        
-        # modify x
-        
-        x_start[833 + start_square_index] = 1
-        
-        # get end square
-        
-        y_end = end_model.predict([x_start])
-        end_square_list = y_end[0]
-        
-        while True:
-        
-            if wrong_start:
-                break
-        
-            end_square_index = np.argmax(end_square_list)
-            end_square = chess.SQUARE_NAMES[end_square_index]
-            
-            move = start_square + end_square
-            
-            try:
-                board.push_uci(move)
-                return move
-            except:
-                
-                try:
-                    board.push_uci(move + "Q")
-                    return move + "Q"
-                except:
-                    pass
-                
-                if end_square_list[end_square_index] <= 0:
-                    wrong_start = True
-                else: 
-                    end_square_list[end_square_index] = 0
-                
-                continue
-        
-        start_square_list[start_square_index] = 0
-        x_start[833 + start_square_index] = 0
-        wrong_start = False
-    
-board = chess.Board()
-
-print("Enter 0 if you would like to play as White, and 1 if you would like to play as Black: ")
-colour = int(input())
-
-if (colour == 0):
-    
-    while (not board.is_checkmate()):
-        
-        try:
+    if (colour == 0):
+        while (not board.is_checkmate()):
             player_move = input()
             board.push_san(player_move)
-        except:
-            print("invalid move, try again")
-            continue
+            
+            legal = [m for m in board.legal_moves]
+            
+            engine_move = random.choice(legal)
+            board.san_and_push(engine_move)
+            print(engine_move)
+        print("The winner is: " + board.outcome().winner)
+            
+    else:
+        while (not board.is_checkmate()):
+            engine_move = random.choice(board.legal_moves)
+            board.push_san(engine_move)
+            print(engine_move)
+            
+            player_move = input()
+            board.push_san(player_move)
+        print("The winner is: " + board.outcome().winner)
         
-        if board.is_checkmate():
-            break
-        
-        engine_move = get_move(board)
-        print(engine_move)
-        
-    print(board.outcome().result())
-        
+    print("Thank you for playing")
+
 else:
     
-    while (not board.is_checkmate()):
+    if difficulty == 1:
+        start_model = keras.models.load_model('start_net_bigset')
+        end_model = keras.models.load_model('end_net_bigset')
+    else:
+        start_model = keras.models.load_model('start_net_bigset')
+        end_model = keras.models.load_model('end_net_bigset')
+
+    def get_net_input(fen):
         
-        engine_move = get_move(board)
-        print(engine_move)
+        input = []
+        last = False
         
-        if board.is_checkmate():
-            break
+        for c in fen:
+            
+            # B or W to move
+            if last:
+                
+                if c == 'w':
+                    input.append(0)
+                else:
+                    input.append(1)
+                break
+            
+            # ignore
+            if (c == '/'):
+                continue
+            
+            # just before to move information
+            elif (c == ' '):
+                last = True
+                continue
+            
+            # data about square
+            else:
+                
+                vec = np.zeros(13) # empty square, K, Q, B, N, R, P, k, q, b, n, r, p
+                
+                # empty square
+                try:
+                    
+                    x = int(c)
+                    
+                    vec[0] = 1
+                    for i in range(x):
+                        for n in vec:
+                            input.append(int(n))
+                        
+                # piece
+                except:
+                    
+                    if (c == 'K'):
+                        vec[1] = 1
+                    elif (c == 'Q'):
+                        vec[2] = 1
+                    elif (c == 'B'):
+                        vec[3] = 1
+                    elif (c == 'N'):
+                        vec[4] = 1
+                    elif (c == 'R'):
+                        vec[5] = 1
+                    elif (c == 'P'):
+                        vec[6] = 1
+                    elif (c == 'k'):
+                        vec[7] = 1
+                    elif (c == 'q'):
+                        vec[8] = 1
+                    elif (c == 'b'):
+                        vec[9] = 1
+                    elif (c == 'n'):
+                        vec[10] = 1
+                    elif (c == 'r'):
+                        vec[11] = 1
+                    elif (c == 'p'):
+                        vec[12] = 1
+                    
+                    for n in vec:
+                        input.append(int(n))
+        
+        return input
+
+    def get_move(board):
+        
+        wrong_start = False
+        
+        # get starting square
+        
+        fen = board.fen()
+        x_start = get_net_input(fen)
+        
+        y_start = start_model.predict([x_start])
+        start_square_list = y_start[0]
+        
+        for i in range(64):
+            x_start.append(0)
         
         while True:
+        
+            start_square_index = np.argmax(start_square_list)
+            start_square = chess.SQUARE_NAMES[start_square_index]
+            
+            # modify x
+            
+            x_start[833 + start_square_index] = 1
+            
+            # get end square
+            
+            y_end = end_model.predict([x_start])
+            end_square_list = y_end[0]
+            
+            while True:
+            
+                if wrong_start:
+                    break
+            
+                end_square_index = np.argmax(end_square_list)
+                end_square = chess.SQUARE_NAMES[end_square_index]
+                
+                move = start_square + end_square
+                
+                try:
+                    board.push_uci(move)
+                    return move
+                except:
+                    
+                    try:
+                        board.push_uci(move + "Q")
+                        return move + "Q"
+                    except:
+                        pass
+                    
+                    if end_square_list[end_square_index] <= 0:
+                        wrong_start = True
+                    else: 
+                        end_square_list[end_square_index] = 0
+                    
+                    continue
+            
+            start_square_list[start_square_index] = 0
+            x_start[833 + start_square_index] = 0
+            wrong_start = False
+        
+    board = chess.Board()
+
+    print("Enter 0 if you would like to play as White, and 1 if you would like to play as Black: ")
+    colour = int(input())
+
+    if (colour == 0):
+        
+        while (not board.is_checkmate()):
+            
             try:
                 player_move = input()
                 board.push_san(player_move)
-                break
             except:
                 print("invalid move, try again")
                 continue
+            
+            if board.is_checkmate():
+                break
+            
+            engine_move = get_move(board)
+            print(engine_move)
+            
+        print(board.outcome().result())
+            
+    else:
         
-    print(board.outcome().result())
-    
-print("Thank you for playing")
+        while (not board.is_checkmate()):
+            
+            engine_move = get_move(board)
+            print(engine_move)
+            
+            if board.is_checkmate():
+                break
+            
+            while True:
+                try:
+                    player_move = input()
+                    board.push_san(player_move)
+                    break
+                except:
+                    print("invalid move, try again")
+                    continue
+            
+        print(board.outcome().result())
+        
+    print("Thank you for playing")
